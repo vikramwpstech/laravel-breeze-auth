3adb7d8b3aa8bf8b7cc1df8a4ba8157c1c97d3dc
vikramwpstech <vikramwpstechnologies@gmail.com>
2023-07-19 12:34:59 +0530

    path correction and disable admin register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,9 +44,9 @@
                         @else
                             <a href="{{ route('admin.login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Admin Log in</a>
 
-                            @if (Route::has('register'))
+                            {{-- @if (Route::has('register'))
                                 <a href="{{ route('admin.register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Admin Register</a>
-                            @endif
+                            @endif --}}
                         @endauth
                     </div>
                 @endif
